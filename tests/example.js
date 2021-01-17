61c97601d9eef41e1a5a08e9c58d044703764505
Jacopo Ferrazza <j.ferrazza@gbh.com.do>
2021-01-17 14:03:46 -0400

    added clouldbuild file

--- tests/example.js
+++ tests/example.js
@@ -1,9 +1,12 @@
 const puppeteer = require('puppeteer');
+const iPhone = puppeteer.devices['iPhone 6'];
+
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.emulate(iPhone);
+  await page.goto('https://google.com');
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
